c8f49123a48d8d37c82bd97d17a64d7594e7c3d6
jbenguira <z51biz@gmail.com>
2022-08-18 20:39:12 +0300

    increased wait time

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -2,7 +2,7 @@
 set -o allexport; source .env; set +o allexport;
 
 echo "Waiting for Metabase to be ready"
-sleep 10s;
+sleep 30s;
 
 app_target=$(docker-compose port metabase 3000)
 export SETUP_TOKEN=$(curl -s -m 5 -X GET -H "Content-Type: application/json" http://$app_target/api/session/properties | jq -r '.["setup-token"]')
