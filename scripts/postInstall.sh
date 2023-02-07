084888910b7b354e3f64e8931af46cf08b60a6ea
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-07 13:44:51 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -2,7 +2,7 @@
 set -o allexport; source .env; set +o allexport;
 
 echo "Waiting for Metabase to be ready"
-sleep 30s;
+sleep 90s;
 
 app_target=$(docker-compose port metabase 3000)
 export SETUP_TOKEN=$(curl -s -m 5 -X GET -H "Content-Type: application/json" http://$app_target/api/session/properties | jq -r '.["setup-token"]')
