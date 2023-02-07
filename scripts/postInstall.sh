d046a95310a543f0d675bb5a690a0bc4d05debed
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-07 14:06:52 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,26 +4,31 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for Metabase to be ready"
 sleep 30s;
 
-# app_target=$(docker-compose port metabase 3000)
+app_target=$(docker-compose port metabase 3000)
 
-# curl http://$app_target/
+curl http://$app_target/
 
-# sleep 10s;
+sleep 10s;
 
-# SETUP_TOKEN=$(curl -s -m 5 -X GET -H "Content-Type: application/json" http://$app_target/api/session/properties | jq -r '.["setup-token"]')
+SETUP_TOKEN=$(curl -s -m 5 -X GET -H "Content-Type: application/json" http://$app_target/api/session/properties | jq -r '.["setup-token"]')
 
-# curl --header "Content-Type: application/json" \
-# http://$app_target/api/setup \
-#  --request POST -d '{
-#     "token": "'${SETUP_TOKEN}'",
-#     "user": {
-#         "email": "'${ADMIN_EMAIL}'",
-#         "first_name": "Metabase",
-#         "last_name": "Admin",
-#         "password": "'${ADMIN_PASSWORD}'"
-#     },
-#     "prefs": {
-#         "allow_tracking": false,
-#         "site_name": "Metabase Instance"
-#     }
-# }'
+echo ${SETUP_TOKEN}
+
+curl --header "Content-Type: application/json" \
+http://$app_target/api/setup \
+ --request POST -d '{
+    "token": "'${SETUP_TOKEN}'",
+    "user": {
+        "email": "'${ADMIN_EMAIL}'",
+        "first_name": "Metabase",
+        "last_name": "Admin",
+        "password": "'${ADMIN_PASSWORD}'",
+        "password_confirm": "'${ADMIN_PASSWORD}'",
+        "site_name": "Metabase Instance"
+    },
+    "prefs": {
+        "allow_tracking": false,
+        "site_name": "Metabase Instance",
+        "site_locale": "en"
+    }
+}'
