66c447a8b23ab1c6650cffc62e46018f38a20922
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-07 14:12:31 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,14 +5,7 @@ echo "Waiting for Metabase to be ready"
 sleep 30s;
 
 app_target=$(docker-compose port metabase 3000)
-
-curl http://$app_target/
-
-sleep 10s;
-
-SETUP_TOKEN=$(curl -s -m 5 -X GET -H "Content-Type: application/json" http://$app_target/api/session/properties | jq -r '.["setup-token"]')
-
-echo ${SETUP_TOKEN}
+export SETUP_TOKEN=$(curl -s -m 5 -X GET -H "Content-Type: application/json" http://$app_target/api/session/properties | jq -r '.["setup-token"]')
 
 curl --header "Content-Type: application/json" \
 http://$app_target/api/setup \
@@ -22,13 +15,10 @@ http://$app_target/api/setup \
         "email": "'${ADMIN_EMAIL}'",
         "first_name": "Metabase",
         "last_name": "Admin",
-        "password": "'${ADMIN_PASSWORD}'",
-        "password_confirm": "'${ADMIN_PASSWORD}'",
-        "site_name": "Metabase Instance"
+        "password": "'${ADMIN_PASSWORD}'"
     },
     "prefs": {
         "allow_tracking": false,
-        "site_name": "Metabase Instance",
-        "site_locale": "en"
+        "site_name": "Metabase Instance"
     }
 }'
